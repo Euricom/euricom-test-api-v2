de306ede1f0a9525b22552ebc1f4252e8de6aa4d
Peter Cosemans <peter.cosemans@gmail.com>
2023-04-04 19:44:44 +0200

    refactor: use mdx instead of md

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,30 @@
+import mdx from "@next/mdx";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
+const withMDX = mdx({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
     appDir: true,
   },
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
   async redirects() {
     return [
       {
@@ -37,4 +53,4 @@ const config = {
     defaultLocale: "en",
   },
 };
-export default config;
+export default withMDX(config);
---
+++ src/components/MarkdownLayout.tsx
@@ -0,0 +1,5 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="prose m-5">{children}</div>;
+};
+
+export default Layout;
