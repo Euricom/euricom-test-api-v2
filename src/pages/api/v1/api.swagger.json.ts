60485e251806565bab421dda2fadbb4a91f005f9
Peter Cosemans <peter.cosemans@gmail.com>
2023-04-04 19:57:05 +0200

    fix: swagger json is incomplete

--- src/pages/api/v1/api.swagger.json.ts
+++ src/pages/api/v1/api.swagger.json.ts
@@ -1,4 +1,8 @@
 import { withSwagger } from "next-swagger-doc";
+import { generateSchema } from "@anatine/zod-openapi";
+import { taskSchema } from "@/server/taskRepo";
+import { userSchema } from "@/server/userRepo";
+import { productSchema } from "@/server/productRepo";
 import pkg from "../../../../package.json";
 
 const swaggerHandler = withSwagger({
@@ -8,7 +12,30 @@ const swaggerHandler = withSwagger({
       title: "Euricom Test API - V2",
       version: pkg.version,
     },
+    components: {
+      schemas: {
+        task: generateSchema(taskSchema),
+        user: generateSchema(userSchema),
+        product: generateSchema(productSchema),
+        basket: generateSchema(productSchema),
+        error: {
+          type: "object",
+          properties: {
+            error: {
+              type: "string",
+            },
+            message: {
+              type: "string",
+            },
+            details: {
+              required: false,
+              type: "string",
+            },
+          },
+        },
+      },
+    },
   },
-  apiFolder: "src/app/api",
+  apiFolder: "src/pages/api",
 });
 export default swaggerHandler();
