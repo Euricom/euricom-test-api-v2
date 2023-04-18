1af81dba737bfa2423f8a4f1852df4c564639f3a
Peter Cosemans <peter.cosemans@gmail.com>
2023-04-18 15:36:32 +0200

    add cors support

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,25 @@ const config = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/v1/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
